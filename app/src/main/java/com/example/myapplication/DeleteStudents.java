aa2db97dfb9020bbdea0f19865cdc4046cfecd14
Andriy <ak8022@bs.amalnet.k12.il>
2021-02-12 22:43:04 +0200

    A commit

--- app/src/main/java/com/example/myapplication/DeleteStudents.java
+++ app/src/main/java/com/example/myapplication/DeleteStudents.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,7 @@ public class DeleteStudents extends AppCompatActivity
     SQLiteDatabase db;
     Cursor crsr;
     ArrayList<String> tbl = new ArrayList<>();
+    ListView studentsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,7 +27,10 @@ public class DeleteStudents extends AppCompatActivity
         hlp=new HelperDB(this);
         db=hlp.getWritableDatabase();
 
+        studentsList=(ListView)findViewById(R.id.studentsList);
+
         read();
+
     }
 
     public void read()
@@ -41,11 +47,14 @@ public class DeleteStudents extends AppCompatActivity
             String name = crsr.getString(nameCol);
             boolean active = crsr.getInt(activeCol)>0;
             if (!active) continue;
-            String tmp=""+key+", "+name+", "+active;
+            String tmp=name;
             tbl.add(tmp);
             crsr.moveToNext();
         }
         crsr.close();
         db.close();
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, tbl);
+        studentsList.setAdapter(adp);
     }
 }
