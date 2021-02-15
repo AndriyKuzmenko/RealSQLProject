231238f4e1ba03a7dda8ea53f5fd67a474eed51c
Andriy <ak8022@bs.amalnet.k12.il>
2021-02-15 19:55:51 +0200

    A commit

--- app/src/main/java/com/example/myapplication/AddGrades.java
+++ app/src/main/java/com/example/myapplication/AddGrades.java
@@ -3,14 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddGrades extends AppCompatActivity
 {
+    EditText student,address,subject,grade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grades);
+
+        student=(EditText)findViewById(R.id.student);
+        address=(EditText)findViewById(R.id.address);
+        subject=(EditText)findViewById(R.id.subject);
+        grade=(EditText)findViewById(R.id.grade);
     }
 }
