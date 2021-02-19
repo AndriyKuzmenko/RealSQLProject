e78814423ba98906e4098a5f0342a8ab90354892
Andriy <ak8022@bs.amalnet.k12.il>
2021-02-19 20:23:47 +0200

    A commit

--- app/src/main/java/com/example/myapplication/UpdateActivity.java
+++ app/src/main/java/com/example/myapplication/UpdateActivity.java
@@ -2,15 +2,64 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class UpdateActivity extends AppCompatActivity
 {
+    SQLiteDatabase db;
+    HelperDB hlp;
+    Cursor crsr;
+    ArrayList<String> nameTBL;
+    EditText nameUpdate, addressUpdate, phoneUpdate, homePhoneUpdate, fatherUpdate, fatherPhoneUpdate, motherUpdate, motherPhoneUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        hlp = new HelperDB(this);
+        db = hlp.getWritableDatabase();
+        db.close();
+        nameTBL=new ArrayList<>();
+
+        nameUpdate=(EditText)findViewById(R.id.nameUpdate);
+        addressUpdate=(EditText)findViewById(R.id.addressUpdate);
+        phoneUpdate=(EditText)findViewById(R.id.phoneUpdate);
+        homePhoneUpdate=(EditText)findViewById(R.id.homePhoneUpdate);
+        fatherUpdate=(EditText)findViewById(R.id.fatherUpdate);
+        fatherPhoneUpdate=(EditText)findViewById(R.id.fatherPhoneUpdate);
+        motherUpdate=(EditText)findViewById(R.id.motherUpdate);
+        motherPhoneUpdate=(EditText)findViewById(R.id.motherPhoneUpdate);
+    }
+
+    /**
+     * Reads all the information about the students from the SQL file and puts it in an arrayList.
+     */
+
+    public void read()
+    {
+        db=hlp.getWritableDatabase();
+        crsr=db.query(Users.TABLE_USERS, null, null, null, null, null, null);
+        int idCol=crsr.getColumnIndex(Users.KEY_ID);
+        int nameCol=crsr.getColumnIndex(Users.NAME);
+
+        crsr.moveToFirst();
+        while (!crsr.isAfterLast())
+        {
+            int key=crsr.getInt(idCol);
+            String name = crsr.getString(nameCol);
+
+            String temp=key+",   "+name;
+            nameTBL.add(temp);
+            crsr.moveToNext();
+        }
+        crsr.close();
+        db.close();
     }
 }
